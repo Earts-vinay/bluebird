ac73f285c7ab1c8e6dc3a4d72195baceb6d8362f
Earts-vinay <vinay.r@entitledarts.com>
2024-07-24 12:03:16 +0530

    sidenav changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CssBaseline, Box } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard';
+import Devices from './pages/Devices';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Sidenav from './components/Sidenav';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Box sx={{ display: 'flex' }}>
+        <Sidenav />
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: '#f0f2f5', p: 3,  }}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/devices" element={<Devices />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
   );
 }
 
---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Avatar, Typography, Divider, Box, Menu, MenuItem, FormControl, TextField } from '@mui/material';
+import { Dashboard, Devices, People, Settings, Logout } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+const Sidebar = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [activeItem, setActiveItem] = useState('/');
+    const open = Boolean(anchorEl);
+    const now = moment();
+    const formattedDate = now.format('ddd-DD/MM/YY');
+    const time = now.format('HH:mm:ss');
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleItemClick = (path) => {
+        setActiveItem(path);
+    };
+
+    const getItemStyle = (path) => ({
+        backgroundColor: activeItem === path ? 'rgba(24, 123, 205, 0.6)' : 'transparent',
+        color: activeItem === path ? '#187BCD' : '#fff',
+        borderRadius: "10px"
+    });
+
+    return (
+        <Drawer
+            variant="permanent"
+            sx={{
+                width: 240,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: 240,
+                    boxSizing: 'border-box',
+                    background: 'linear-gradient(140deg, #00101E 0%, #023664 100%, #03254C 100%)',
+                    color: '#fff',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                },
+            }}
+        >
+            <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ position: 'sticky', top: 0, zIndex: 1, py: 1 }}>
+                    <ListItem sx={{ display: "flex", justifyContent: "center", alignItems: "center", color: "#5BB5FF" }}>
+                        <Typography variant="h6">COMPANY NAME</Typography>
+                    </ListItem>
+                    <ListItem sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <FormControl sx={{ width: "12rem" }}>
+                            <TextField
+                                select
+                                size='small'
+                                fullWidth
+                                label="Property Name"
+                                sx={{
+                                    backgroundColor: "#1C2E3D",
+                                    color: "white",
+                                    borderRadius: "5px",
+                                    '& .MuiInputBase-root': {
+                                        color: 'white',
+                                    },
+                                }}
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </TextField>
+                        </FormControl>
+                    </ListItem>
+                    <Divider sx={{ borderColor: 'rgba(255, 255, 255, 0.48)', mx: 3,mt:3,mb:2 }} />
+                </Box>
+                <Box sx={{ flexGrow: 1, overflowY: 'auto' }}>
+                    <List>
+                        <ListItem>
+                            <Avatar sx={{ bgcolor: 'grey', width: "65px", height: "65px" }}>JD</Avatar>
+                            <div style={{ marginLeft: '16px' }}>
+                                <Typography variant='p'>Welcome,<br /> James Dho</Typography> <br />
+                                <Typography variant='p'>{formattedDate}</Typography>
+                            </div>
+                        </ListItem>
+                        <Box sx={{ pt: 3, mx: 2 }}>
+                            <ListItem
+                                button
+                                component={Link}
+                                to="/"
+                                onClick={() => handleItemClick('/')}
+                                sx={getItemStyle('/')}
+                            >
+                                <ListItemIcon><Dashboard style={{ color: '#fff' }} /></ListItemIcon>
+                                <ListItemText primary="Dashboard" />
+                            </ListItem>
+                            <ListItem
+                                button
+                                component={Link}
+                                to="/devices"
+                                onClick={() => handleItemClick('/devices')}
+                                sx={getItemStyle('/devices')}
+                            >
+                                <ListItemIcon><Devices style={{ color: '#fff' }} /></ListItemIcon>
+                                <ListItemText primary="Devices" />
+                            </ListItem>
+                            <ListItem
+                                button
+                                component={Link}
+                                to="/users"
+                                onClick={() => handleItemClick('/users')}
+                                sx={getItemStyle('/users')}
+                            >
+                                <ListItemIcon><People style={{ color: '#fff' }} /></ListItemIcon>
+                                <ListItemText primary="Users" />
+                            </ListItem>
+                            <ListItem
+                                button
+                                component={Link}
+                                to="/settings"
+                                onClick={() => handleItemClick('/settings')}
+                                sx={getItemStyle('/settings')}
+                            >
+                                <ListItemIcon><Settings style={{ color: '#fff' }} /></ListItemIcon>
+                                <ListItemText primary="Settings" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon><Logout style={{ color: '#fff' }} /></ListItemIcon>
+                                <ListItemText primary="Logout" />
+                            </ListItem>
+                        </Box>
+                    </List>
+                </Box>
+                <Box sx={{ position: 'sticky', bottom: 0, textAlign: 'center', py: 2 }}>
+                    <Box sx={{ py: 2 }}>
+                        <img src="/assets/logo.svg" alt="Logo" style={{ maxWidth: '100%', maxHeight: '50px' }} />
+                    </Box>
+                </Box>
+            </Box>
+        </Drawer>
+    );
+};
+
+export default Sidebar;
