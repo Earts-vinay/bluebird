efd24117e2edcec18364757a3f5fcb366a803711
Earts-vinay <vinay.r@entitledarts.com>
2024-07-31 12:25:02 +0530

    changes on styles and actions

--- src/pages/Devices.jsx
+++ src/pages/Devices.jsx
@@ -1,24 +1,12 @@
 import React, { useState } from 'react';
-import {
-  Container,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Typography,
-  Box,
-  Button
-} from '@mui/material';
+import { Typography, Box, } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import CustomSearch from '../utils/CustomSearch'; 
+import CustomSearch from '../utils/CustomSearch';
 import { useNavigate } from 'react-router-dom';
 import DefaultTable from '../utils/DefaultTable';
 
-const tableHeadings =['Property Name', 'Raise Alerts', 'Cameras Installed', 'Active Cameras', 'Inactive Cameras',];
-const columns = ['propertyName', 'raiseAlerts', 'camerasInstalled', 'activeCameras', 'inactiveCameras', ];
+const tableHeadings = ['Property Name', 'Raise Alerts', 'Cameras Installed', 'Active Cameras', 'Inactive Cameras',];
+const columns = ['propertyName', 'raiseAlerts', 'camerasInstalled', 'activeCameras', 'inactiveCameras',];
 const rows = [
   { propertyName: "Ikea Test 01", raiseAlerts: 1, camerasInstalled: 0, activeCameras: 3, inactiveCameras: 1, propertyLocation: 'Virginia, USA.' },
   { propertyName: "Ikea Test 02", raiseAlerts: 1, camerasInstalled: 0, activeCameras: 3, inactiveCameras: 1, propertyLocation: 'Virginia, USA.' },
@@ -33,15 +21,14 @@ const rows = [
   { propertyName: "Ikea Test 07", raiseAlerts: 1, camerasInstalled: 0, activeCameras: 3, inactiveCameras: 1, propertyLocation: 'Virginia, USA.' },
   { propertyName: "Ikea Test 07", raiseAlerts: 1, camerasInstalled: 0, activeCameras: 3, inactiveCameras: 1, propertyLocation: 'Virginia, USA.' },
 
-
 ];
 
 const Devices = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedRowIndex, setSelectedRowIndex] = useState(0); 
+  const [selectedRowIndex, setSelectedRowIndex] = useState(0);
   const navigate = useNavigate();
-  const LocationIconUrl =  process.env.PUBLIC_URL + '/assets/icons/location.svg';
-  
+  const LocationIconUrl = process.env.PUBLIC_URL + '/assets/icons/location.svg';
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -50,52 +37,34 @@ const Devices = () => {
     item.propertyName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const handleGoBack  = () => {
-    navigate(-1); 
+  const handleGoBack = () => {
+    navigate(-1);
   };
 
   const handleRowClick = (index) => {
-    setSelectedRowIndex(index);    
+    setSelectedRowIndex(index);
     navigate("/devices/devicedetails")
-    
+
   };
 
   return (
     < >
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Box display="flex" alignItems="center" >
-          <Button
-            onClick={handleGoBack }
-            sx={{
-              borderRadius: '50%',
-              width: 35,
-              height: 35,
-              minWidth: 0,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              backgroundColor: 'rgba(233, 244, 251, 1)',
-              color: 'white',
-              // '&:hover': { backgroundColor: 'darkblue' }
-            }}
-          >
+          <Box onClick={handleGoBack} className="backButtonStyle">
             <ArrowBackIosIcon sx={{ color: '#3275AF', paddingLeft: 1, fontSize: 18 }} />
-          </Button>
-          <Typography variant="h6" ml={2} sx={{ color: "#3275AF", fontSize:"18px" }}>
+          </Box>
+          <Typography variant="h6" ml={2} sx={{ color: "#3275AF", fontSize: "18px" }}>
             Devices
           </Typography>
         </Box>
-        <CustomSearch
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search..."
-        />
+        <CustomSearch value={searchTerm} onChange={handleSearchChange} placeholder="Search..." />
       </Box>
 
-      <Box sx={{pt:4}}>
-      <DefaultTable columns={columns} rows={filteredData} tableHeadings={tableHeadings} onClick={handleRowClick}/>
+      <Box sx={{ pt: 5 }}>
+        <DefaultTable columns={columns} rows={filteredData} tableHeadings={tableHeadings} onClick={handleRowClick} />
       </Box>
-    
+
     </>
   );
 };
